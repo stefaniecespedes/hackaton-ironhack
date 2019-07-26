da55ad5d1991b67570105566303ac984c412c558
dgentil13 <ahoydanni3@gmail.com>
2019-07-26 09:54:36 -0300

    Map

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router  = express.Router();
+const Stores = require('../models/stores');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/api', (req, res, next) => {
+
+  Stores.find().then(store => {
+    res.send(store)
+  }).catch(err => console.log(err));
 });
 
 module.exports = router;
