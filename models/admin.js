316a0cda72103c5e94ba67fe99c94769e8213b0a
Uira Oliveira <uira.sampaio@hotmail.com>
2019-07-26 11:10:38 -0300

    passport and admin profile

---
+++ models/admin.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const adminSchema = new Schema({
+  username: { type: String },
+  password: { type: String },
+});
+
+const Admin = mongoose.model('Admin', adminSchema);
+
+module.exports = Admin;
